9fd9d08ac85fdb601318333cd290a6ca2ab00626
prjctimg <prjctimg@outlook.com>
2023-10-15 03:48:10 +0200

    Modified siteMetaData. Added headerNavLinks

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,10 @@
+import { PenTool, Code, Wind } from 'react-feather'
+
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/', icon: <Wind className="m-4 inline text-gray-600" />, title: 'Getting Started' },
+  { href: '/blog', title: 'API', icon: <PenTool className="m-4 inline text-gray-600" /> },
+
+  { href: '/projects', icon: <Code className="m-4 inline text-gray-600" />, title: 'Guides' },
 ]
 
 export default headerNavLinks
